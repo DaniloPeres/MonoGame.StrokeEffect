a3cc53a2ba4f18ef4cd62779cb65553532e237a2
Danilo Peres <danilo_meirelles@hotmail.com>
2021-05-19 22:50:44 +0200

    Fix the empty text

--- MonoGame.StrokeEffect/StrokeEffectSpriteFont.cs
+++ MonoGame.StrokeEffect/StrokeEffectSpriteFont.cs
@@ -9,6 +9,8 @@ namespace MonoGame
 {
     public static partial class StrokeEffect
     {
+        public static Texture2D transparentPixel;
+
         public static Texture2D CreateStrokeSpriteFont(SpriteFont spriteFont, string text, Color textColor, Vector2 scale, int strokeSize, Color strokeColor, GraphicsDevice graphics, StrokeType strokeType = StrokeType.OutlineAndTexture)
         {
             using (var textTexture2D = DrawSpriteFontToTexture2D(spriteFont, text, textColor, scale, graphics))
@@ -19,9 +21,12 @@ namespace MonoGame
 
         private static Texture2D DrawSpriteFontToTexture2D(SpriteFont spriteFont, string text, Color textColor, Vector2 scale, GraphicsDevice graphics)
         {
+            var textSize = spriteFont.MeasureString(text) * scale;
+            if (textSize.X == 0 || textSize.Y == 0)
+                return GetTransparentPixel(graphics);
+
             lock (graphics)
             {
-                var textSize = spriteFont.MeasureString(text) * scale;
                 var target = new RenderTarget2D(graphics, (int)textSize.X, (int)textSize.Y);
                 graphics.SetRenderTarget(target);// Now the spriteBatch will render to the RenderTarget2D
                 graphics.Clear(Color.Transparent);
@@ -32,6 +37,19 @@ namespace MonoGame
                 graphics.SetRenderTarget(null);//This will set the spriteBatch to render to the screen again.
                 return target;
             }
+        }
+
+        private static Texture2D GetTransparentPixel(GraphicsDevice graphics)
+        {
+            if (transparentPixel == null)
+            {
+                transparentPixel = new Texture2D(graphics, 1, 1);
+                var color = new Color[1];
+                color[0] = Color.Transparent;
+                transparentPixel.SetData<Color>(color);
+            }
+
+            return transparentPixel;
         }
     }
 }
